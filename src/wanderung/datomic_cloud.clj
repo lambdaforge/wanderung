172ed619dc9c041aa2cb2698dfa33a022b8b0e4f
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-02-21 14:59:52 +0100

    fix schema-attributes

--- src/wanderung/datomic_cloud.clj
+++ src/wanderung/datomic_cloud.clj
@@ -9,15 +9,18 @@
                                   [?tx :db/txInstant ?inst]
                                   [(< ?bf ?inst)]]
                                 db
-                                (java.util.Date. 70)))]
+                                (java.util.Date. 70)))
+        schema-attrs #{:db/cardinality :db/valueType :db/unique}
+        id->ident (->> (d/q '[:find ?e ?id :where [?e :db/ident ?id]] db)
+                       (into {}))]
     (apply concat (for [[tid tinst] txs]
-                    (into [[tid :db/txInstant tinst tid true]]
-                          (sort-by
-                            first
-                            (d/q '[:find ?e ?at ?v ?t ?added
-                                   :in $ ?t
-                                   :where
-                                   [?e ?a ?v ?t ?added]
-                                   [?a :db/ident ?at]
-                                   (not [?a :db/ident :db/txInstant])]
-                                 (d/history db) tid)))))))
+                    (->> (d/q '[:find ?e ?at ?v ?t ?added
+                                :in $ ?t
+                                :where
+                                [?e ?a ?v ?t ?added]
+                                [?a :db/ident ?at]
+                                (not [?a :db/ident :db/txInstant])]
+                              (d/history db) tid)
+                         (map (fn [[_ a _ _ _ :as entity]] (if (schema-attrs a) (update entity 2 id->ident) entity)))
+                         (sort-by first)
+                         (into [[tid :db/txInstant tinst tid true]]))))))
