3d476452d7a6c46f8b0f207d516d2d5dad93e27f
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-03-04 15:00:07 +0100

    complete api

--- src/wanderung/db/api.clj
+++ src/wanderung/db/api.clj
@@ -1,49 +1,115 @@
 (ns wanderung.db.api
+  (:refer-clojure :exclude [sync])
   (:require [datomic.client.api :as dc]
             [datahike.api :as dh]))
 
+(defn not-supported []
+  (throw (ex-info "Not supported yet." {:error/cause :not-supported})))
+
 (defprotocol Historian
+  (db-stats [db])
   (history [db])
-  (as-of [db time-point]))
+  (as-of [db time-point])
+  (since [db time-point])
+  (with [db arg-map]))
+
+(defprotocol Searcher
+  (datoms [db arg-map])
+  (pull [db arg-map] [db selector eid])
+  (index-range [db arg-map]))
 
 (defrecord DatahikeDb [state]
   Historian
+  (db-stats [db] (not-supported))
   (history [db] (map->DatahikeDb {:state (dh/history state)}))
-  (as-of [db time-point] (map->DatahikeDb {:state (dh/as-of state time-point)})))
+  (as-of [db time-point] (map->DatahikeDb {:state (dh/as-of state time-point)}))
+  (since [db time-point] (map->DatahikeDb {:state (dh/since state time-point)}))
+  (with [db {:keys [tx-data]}]
+    (dh/with state tx-data))
+
+  Searcher
+  (datoms [db {:keys [index components]}]
+    (dh/datoms state index components))
+  (pull [db {:keys [selector eid]}]
+    (dh/pull state selector eid))
+  (pull [db selector eid]
+    (dh/pull state selector eid))
+  (index-range [db arg-map] (not-supported)))
 
 (defrecord DatomicDb [state]
   Historian
+  (db-stats [db] (not-supported))
   (history [db] (map->DatomicDb {:state (dc/history state)}))
-  (as-of [db time-point] (map->DatomicDb {:state (dc/as-of state time-point)})))
+  (as-of [db time-point] (map->DatomicDb {:state (dc/as-of state time-point)}))
+  (since [db time-point] (map->DatomicDb {:state (dc/since state time-point)}))
+  (with [db arg-map] (dc/with state arg-map))
+
+  Searcher
+  (datoms [db arg-map] (dc/datoms db arg-map))
+  (pull [db arg-map] (dc/pull state arg-map))
+  (pull [db selector eid] (dc/pull state selector eid))
+  (index-range [db arg-map] (not-supported)))
 
 (defprotocol Transactor
   (transact [conn arg-map])
-  (db [conn]))
+  (release [conn])
+  (db [conn])
+  (with-db [conn])
+  (sync [conn time-point])
+  (tx-range [conn arg-map]))
 
 (defrecord DatahikeConnection [state]
   Transactor
-  (transact [conn arg-map]
-    (dh/transact! state arg-map))
-  (db [conn] (map->DatahikeDb {:state @state})))
+  (transact [conn arg-map] (dh/transact! state arg-map))
+  (release [conn] (dh/release state))
+  (db [conn] (map->DatahikeDb {:state @state}))
+  (with-db [conn] @state)
+  (sync [conn time-point] (not-supported))
+  (tx-range [conn arg-map] (not-supported)))
 
 (defrecord DatomicConnection [state]
   Transactor
-  (transact [conn arg-map]
-    (dc/transact state arg-map))
-  (db [conn] (map->DatomicDb {:state (dc/db state)})))
+  (transact [conn arg-map] (dc/transact state arg-map))
+  (release [conn])
+  (db [conn] (map->DatomicDb {:state (dc/db state)}))
+  (with-db [conn] (dc/with-db state))
+  (sync [conn time-point] (not-supported))
+  (tx-range [conn arg-map] (not-supported)))
 
 (defprotocol Connector
   (connect [client arg-map]))
 
+(defprotocol Creator
+  (administer-system [client])
+  (list-databases [client arg-map] )
+  (create-database [client arg-map])
+  (delete-database  [client arg-map]))
+
 (defrecord DatahikeClient [state]
   Connector
   (connect [client arg-map]
-    (map->DatahikeConnection {:state (dh/connect state)})))
+    (map->DatahikeConnection {:state (dh/connect state)}))
+
+  Creator
+  (administer-system [client] (not-supported))
+  (list-databases [client arg-map] (not-supported))
+  (create-database [client arg-map]
+    (dh/create-database arg-map))
+  (delete-database [client arg-map]
+    (dh/delete-database arg-map)))
 
 (defrecord DatomicClient [state]
   Connector
   (connect [client arg-map]
-    (map->DatomicConnection {:state (dc/connect state arg-map)})))
+    (map->DatomicConnection {:state (dc/connect state arg-map)}))
+
+  Creator
+  (administer-system [client] (not-supported))
+  (list-databases [client arg-map] (not-supported))
+  (create-database [client arg-map]
+    (dc/create-database state arg-map))
+  (delete-database [client arg-map]
+    (dc/delete-database state arg-map)))
 
 (defn datahike-client [config]
   (map->DatahikeClient {:state config}))
