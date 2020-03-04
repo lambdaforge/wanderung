6774484d01253d9d2dd83bda7bd2dc6917de9d5e
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-03-04 15:02:44 +0100

    update to snapshot 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.lambdaforge/wanderung "0.1.0"
+(defproject io.lambdaforge/wanderung "0.1.1-SNAPSHOT"
   :description "Data migration tool for Datahike"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
