cedb299e1a618a893a53b4970a3c9f6e2fa6d493
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-03-09 14:12:32 +0100

    add commandline arguments

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.lambdaforge/wanderung "0.1.0"
+(defproject io.lambdaforge/wanderung "0.1.0-SNAPSHOT"
   :description "Data migration tool for Datahike"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,6 +6,9 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.replikativ/datahike "0.2.2-SNAPSHOT"]
                  [com.datomic/client-cloud "0.8.78" :scope "provided"]
-                 [com.cognitect/transit-clj "0.8.313"]]
+                 [io.replikativ/datahike-postgres "0.1.0"]
+                 [com.cognitect/transit-clj "0.8.313"]
+                 [org.clojure/tools.cli "1.0.194"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+  :main wanderung.core
   :repl-options {:init-ns wanderung.core})
--- src/wanderung/core.clj
+++ src/wanderung/core.clj
@@ -1,10 +1,47 @@
 (ns wanderung.core
   (:require [datahike.api :as d]
+            [datahike-postgres.core]
             [datomic.client.api :as dt]
-            [wanderung.datomic-cloud :as wdc]))
+            [wanderung.datomic-cloud :as wdc]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :refer [split]]
+            [clojure.java.io :as io])
+  (:gen-class))
 
-(defn datomic-cloud->datahike [datomic-config datomic-name datahike-config]
-  (let [datomic-conn (dt/connect (dt/client datomic-config) {:db-name datomic-name})
+(defmulti migrate (fn [direction source-configuration target-configuration] direction))
+
+(defmethod migrate [:datomic-cloud :datahike] [_ datomic-config datahike-config]
+  (let [datomic-conn (dt/connect (dt/client (dissoc datomic-config :name)) {:db-name (:name datomic-config)})
         datomic-data (wdc/extract-datomic-cloud-data datomic-conn)
         datahike-conn (d/connect datahike-config)]
-    @(d/migrate datahike-conn datomic-data)))
+    @(d/migrate datahike-conn datomic-data)
+    true))
+
+(defmethod migrate :default [direction _ _]
+  (throw (IllegalArgumentException. (str "Direction " direction " not supported."))))
+
+(def cli-options
+  [["-s" "--source SOURCE" "Source EDN configuration file"
+    :parse-fn identity
+    :validate [#(.exists (io/file %)) "Source configuration does not exist."]]
+   ["-t" "--target TARGET" "Target EDN configuration file"
+    :parse-fn identity
+    :validate [#(.exists (io/file %)) "Target configuration does not exist."]]
+   ["-d" "--direction SOURCE_SYSTEM:TARGET_SYSTEM" "Migration directions separated by colon: e.g. datomic-cloud:datahike"
+    :parse-fn #(->> (split % #":") (mapv keyword))
+    :validate [#(-> (methods migrate) keys set (contains? %)) "Direction not allowed."]]
+   ["-h" "--help"]])
+
+(defn -main [& args]
+  (let [{{:keys [source target direction help]} :options summary :summary errors :errors} (parse-opts args cli-options)]
+    (if errors
+      (->> errors (map println) doall)
+      (if help
+        (do
+          (println "Run migrations to datahike from various sources")
+          (println "USAGE:")
+          (println summary))
+        (do
+          (println "➜ Start migrating from " (first direction) "to" (second direction) "...")
+          (migrate direction (-> source slurp read-string) (-> target slurp read-string))
+          (println "  ✓ Done"))))))
