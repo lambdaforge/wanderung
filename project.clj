33fa30dc8b5e322638cf5c14ee9525e19c84fa30
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2020-11-20 14:25:29 +0100

    ignore latest konserve in datahike

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [io.replikativ/datahike "0.3.3-SNAPSHOT"]
                  [io.replikativ/datahike-jdbc "0.1.2-SNAPSHOT"]
+                 [io.replikativ/datahike "0.3.3-SNAPSHOT" :exclusions [io.replikativ/konserve]]
                  [com.datomic/client-cloud "0.8.78" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.313"]
                  [org.clojure/tools.cli "1.0.194"]]
