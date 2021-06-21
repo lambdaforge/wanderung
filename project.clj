98694475a86d204297b607d217da92f8ac788e61
Konrad Kühne <konrad.kuehne@lambdaforge.io>
2021-06-21 12:08:03 +0200

    add Datomic dev-local tests

--- project.clj
+++ project.clj
@@ -7,11 +7,13 @@
                  [io.replikativ/datahike-jdbc "0.1.2-SNAPSHOT"]
                  [io.replikativ/datahike "0.3.3-SNAPSHOT" :exclusions [io.replikativ/konserve]]
                  [com.datomic/client-cloud "0.8.78" :scope "provided"]
-                 [com.datomic/client-pro "0.9.63"]
+                 [com.datomic/client-pro "0.9.63" :scope "provided"]
+                 [com.datomic/dev-local "0.9.232"]
                  [com.cognitect/transit-clj "0.8.313"]
                  [com.taoensso/nippy "3.1.1"]
                  [org.clojure/tools.cli "1.0.206"]]
   :resource-paths ["resources"]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}
+             :test {:dependencies [[lambdaisland/kaocha "1.0.632"]]}}
   :main wanderung.core
   :repl-options {:init-ns wanderung.core})
--- test/wanderung/core_test.clj
+++ test/wanderung/core_test.clj
@@ -1,6 +1,8 @@
 (ns wanderung.core-test
   (:require [clojure.test :refer :all]
             [wanderung.core :as wanderung]
+            [datahike.api :as dh]
+            [datomic.client.api :as dt]
             [clojure.edn :as edn]
             [clojure.java.io :as io])
   (:import [java.io File]))
@@ -20,4 +22,57 @@
     (is (= testdata (wanderung/datoms-from-storage b)))))
 
 
+
+(deftest test-datomic->datahike
+  (let [source-client-cfg {:wanderung/type :datomic
+                           :server-type :dev-local
+                           :storage-dir :mem
+                           :name "wanderung-source-test"
+                           :system "CI"}
+        source-db-cfg {:db-name "wanderung-source-test"}
+        target-cfg {:wanderung/type :datahike
+                    :store {:backend :mem
+                            :id "wanderung-target-test"}}
+        dt-client (dt/client source-client-cfg)
+        _ (dt/delete-database dt-client source-db-cfg)
+        _ (dt/create-database dt-client source-db-cfg)
+        dt-conn (dt/connect dt-client source-db-cfg)
+        _ (dh/delete-database target-cfg)
+        _ (dh/create-database target-cfg)]
+    (dt/transact dt-conn {:tx-data [{:db/ident :person/name
+                                     :db/valueType :db.type/string
+                                     :db/unique :db.unique/identity
+                                     :db/cardinality :db.cardinality/one}
+                                    {:db/ident :person/age
+                                     :db/valueType :db.type/long
+                                     :db/cardinality :db.cardinality/one}
+                                    {:db/ident :person/siblings
+                                     :db/valueType :db.type/ref
+                                     :db/cardinality :db.cardinality/many}]})
+    (dt/transact dt-conn {:tx-data [{:db/id -1
+                                     :person/name "Alice"
+                                     :person/age 25}
+                                    {:db/id -2
+                                     :person/name "Bob"
+                                     :person/age 35}
+                                    {:person/name "Charlie"
+                                     :person/age 45
+                                     :person/siblings [-1 -2]}]})
+    (wanderung/migrate source-client-cfg target-cfg)
+    (testing "test basic data"
+      (letfn [(coerce-result [result]
+                (->> result
+                     (map #(update (first %) :person/siblings set))
+                     set))]
+        (let [dh-conn (dh/connect target-cfg)
+              query '[:find (pull ?e [:person/name :person/age {:person/siblings [:person/name]}])
+                      :where [?e :person/name _]]
+              dt-result (->> (dt/db dt-conn)
+                             (dt/q query)
+                             coerce-result)
+              dh-result (->> (dh/db dh-conn)
+                             (dh/q query)
+                             coerce-result)]
+          (is (= dt-result
+                 dh-result)))))))
 
