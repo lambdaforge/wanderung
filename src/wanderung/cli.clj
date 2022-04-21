c5b46ad2c7883d215b93c64c948c911200b0c77d
Konrad Kühne <kordano@users.noreply.github.com>
2022-04-21 16:29:15 +0200

    Cli tools env vars (#18)
    * use another namespace for migrations
    
    * fix formatting, remove help from migration
    
    * add pure environment variables for source and target
    
    * add env var example to help
    
    * fix formatting

---
+++ src/wanderung/cli.clj
@@ -0,0 +1,59 @@
+(ns wanderung.cli
+  (:require [wanderung.core :as wc]
+            [clojure.set :refer [rename-keys]]
+            [environ.core :refer [env]])
+  (:import [clojure.lang IExceptionInfo]))
+
+(defn- run-it! [f]
+  (try
+    f
+    (catch Throwable t
+      (println (.getMessage t))
+      (when-not (instance? IExceptionInfo t)
+        (.printStackTrace t))
+      (System/exit 1))
+    (finally
+      (shutdown-agents))))
+
+(defn migrate [opts]
+  (let [merged-opts (-> (select-keys env [:wanderung-source :wanderung-target])
+                        (rename-keys  {:wanderung-source :source
+                                       :wanderung-target :target})
+                        (merge opts))]
+    (println "Running with opts: " merged-opts)
+    (run-it! (wc/migration merged-opts))))
+
+(def migration migrate)
+
+(def m migrate)
+
+(defn help
+  ([]
+   (help {}))
+  ([_]
+   (try
+     (println "WANDERUNG")
+     (println "---------")
+     (println "Run migrations with Datahike to and from various sources")
+     (println "USAGE:")
+     (println "clj -Twanderung [function] [function args]")
+     (println "FUNCTIONS:")
+     (println "---------")
+     (println "migrate/migration/m :source SOURCE :target TARGET")
+     (println "Description: Migrates from given source file to a target file. Source and target must be either file path or environment variable. Use WANDERUNG_SOURCE and WANDERUNG_TARGET environment variables if you don't want to use any input.")
+     (println "Example: clj -Twanderung migrate :source '\"./source-cfg.edn\"' :target '\"target-cfg.edn\"'")
+     (println "Example: clj -Twanderung m :source 'SOURCE_CFG' :target 'TARGET_CFG'")
+     (println "Example: WANDERUNG_SOURCE=./source-cfg.edn WANDERUNG_TARGET=./target-cfg.edn clj -Twanderung migrate'")
+     (println "---------")
+     (println "help/h")
+     (println "Description: Prints this lovely help.")
+     (println "Example: clj -Twanderung help")
+     (catch Throwable t
+       (println (.getMessage t))
+       (when-not (instance? IExceptionInfo t)
+         (.printStackTrace t))
+       (System/exit 1))
+     (finally
+       (shutdown-agents)))))
+
+(def h help)
--- src/wanderung/core.clj
+++ src/wanderung/core.clj
@@ -93,55 +93,43 @@
       slurp
       read-string))
 
-(defn help
-  ([]
-   (help {}))
-  ([_]
-   (println "WANDERUNG")
-   (println "---------")
-   (println "Run migrations with Datahike to and from various sources")
-   (println "USAGE:")
-   (println "clj -Twanderung [function] [function args]")
-   (println "FUNCTIONS:")
-   (println "---------")
-   (println "migration :source SOURCE :target TARGET")
-   (println "Description: Migrates from given source file to a target file.")
-   (println "Example: clj -Twanderung migration :source '\"./source-cfg.edn\"' :target '\"target-cfg.edn\"'")
-   (println "---------")
-   (println "help")
-   (println "Description: Prints this lovely help.")
-   (println "Example: clj -Twanderung help")))
+(defn migration [{:keys [source target check]}]
+  (let [read-cfg (fn [cfg] (when (some? cfg)
+                             (if-let [path-from-env (some? (System/getenv cfg))]
+                               path-from-env
+                               (when (.exists (io/file cfg))
+                                 cfg))))]
+    (if-let [source (read-cfg source)]
+      (if-let [target (read-cfg target)]
+        (let [src-cfg  (load-config source)
+              tgt-cfg  (load-config target)
+              src-type (:wanderung/type src-cfg)
+              tgt-type (:wanderung/type tgt-cfg)]
+          (cond
+            (not (multimethod-for-dispatch-value? datoms-from-storage src-type))
+            (println "Cannot use" src-type "as source database.")
 
-(defn migration [{:keys [source target check] show-help :help}]
-  (if show-help
-    (help)
-    (let [src-cfg (load-config source)
-          tgt-cfg (load-config target)
-          src-type (:wanderung/type src-cfg)
-          tgt-type (:wanderung/type tgt-cfg)]
-      (cond
-        (not (multimethod-for-dispatch-value? datoms-from-storage src-type))
-        (println "Cannot use" src-type "as source database.")
+            (not (multimethod-for-dispatch-value? datoms-to-storage tgt-type))
+            (println "Cannot use" tgt-type "as target database.")
 
-        (not (multimethod-for-dispatch-value? datoms-to-storage tgt-type))
-        (println "Cannot use" tgt-type "as target database.")
+            (:wanderung/read-only? tgt-cfg)
+            (println "Cannot migrate to read-only database.")
 
-        (:wanderung/read-only? tgt-cfg)
-        (println "Cannot migrate to read-only database.")
-
-        :else (do
-                (println "➜ Start migrating from" src-type "to" tgt-type "...")
-                (migrate src-cfg tgt-cfg)
-                (println "  ✓ Done")
-                (when check
-                  (if (multimethod-for-dispatch-value? datoms-from-storage tgt-type)
-                    (do
-                      (println "➜ Comparing datoms between source and target...")
-                      (if (datom/similar-datoms? (datoms-from-storage src-cfg)
-                                                 (datoms-from-storage tgt-cfg))
-                        (println "  ✓ Success: Datoms look the same.")
-                        (println "ERROR: The datoms differ between source and target.")))
-                    (println "ERROR: The target does not support reading datoms"))))))))
+            :else (do
+                    (println "➜ Start migrating from" src-type "to" tgt-type "...")
+                    (migrate src-cfg tgt-cfg)
+                    (println "  ✓ Done")
+                    (when check
+                      (if (multimethod-for-dispatch-value? datoms-from-storage tgt-type)
+                        (do
+                          (println "➜ Comparing datoms between source and target...")
+                          (if (datom/similar-datoms? (datoms-from-storage src-cfg)
+                                                     (datoms-from-storage tgt-cfg))
+                            (println "  ✓ Success: Datoms look the same.")
+                            (println "ERROR: The datoms differ between source and target.")))
+                        (println "ERROR: The target does not support reading datoms"))))))
+        (println "ERORR: Invalid target configuration. Must be either file path or environment variable."))
+      (println "ERORR: Invalid source configuration. Must be either file path or environment variable."))))
 
 (defn -main [& args]
   (let [{options :options
